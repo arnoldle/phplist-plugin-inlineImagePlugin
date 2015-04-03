2038e306c88ad83db3334728f884316835bb919d
bst2002git <bst2002@ios.dhs.org>
2015-04-03 15:06:46 +0200

    Update inlineImagePlugin.php
    Some changes if image has no fully qualified url

--- plugins/inlineImagePlugin.php
+++ plugins/inlineImagePlugin.php
@@ -1,4 +1,5 @@
 <?php
+define('MY_WEB_ROOT',$_SERVER['DOCUMENT_ROOT']);
 
 /**
  * inlineImage Plugin v2.0a3
@@ -8,7 +9,7 @@
  * @author    Arnold V. Lesikar
  * @copyright 2014 Arnold V. Lesikar
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -24,7 +25,7 @@
  *
  * For more information about how to use this plugin, see
  * http://resources.phplist.com/plugins/inlineImage .
- * 
+ *
  */
 
 /**
@@ -75,7 +76,7 @@ class inlineImagePlugin extends phplistPlugin
     private $curid;			// ID of the current message being processed
     private $limit;			// Max total size of inline image files attached to a message
     private $imgdirlimit = 2000; 	// Limiting size for image directory in kB before we clean it out
-    
+
     public $image_types = array(	// Taken from class.phplistmailer.php, except don't allow Flash .swf files
                   'gif'  => 'image/gif',
                   'jpg'  => 'image/jpeg',
@@ -86,65 +87,67 @@ class inlineImagePlugin extends phplistPlugin
                   'tif'  => 'image/tiff',
                   'tiff'  => 'image/tiff'
             );
-    
+
+		private $web_root=MY_WEB_ROOT;
+
     /* No longer have pages associated with this plugin! */
     function adminmenu() {
     	return array ();
   	}
-  	  	
+
   	function __construct()
     {
-     	$this->processing_queue = false; 
-     	$this->forwarding_message = false;   	
+     	$this->processing_queue = false;
+     	$this->forwarding_message = false;
 		$this->coderoot = dirname(__FILE__) . '/inlineImagePlugin/';
-		
+
 		if (!is_dir($this->coderoot))
 			mkdir ($this->coderoot);
 		$imagedir = $this->coderoot . "images/";
 		if (!is_dir($imagedir))
 			mkdir ($imagedir);
-		
+
 		if (file_exists($this->coderoot . 'ldaimages.php')) {	// Do we have the pages for our version 1?
 			// Remove old pages
 			unlink($this->coderoot . 'edit.php');
 			unlink($this->coderoot . 'ldaimages.php');
-			
+
 			// Remove old image files as well
 			$files = glob($this->coderoot . 'images/*.*');
 			foreach ($files as $f)
 				unlink ($f);
-		} 
-		    		
+		}
+
 		parent::__construct();
     }
-    
+
     function initialise() {
     	/* Make sure database is up to date */
 		global $table_prefix;
 		$imgtbl = $this->tables['image'];
 		$msgtbl = $this->tables['msg'];
 
-		if ((Sql_Table_exists($imgtbl) && (!Sql_Table_Column_Exists($imgtbl, "cksum"))) 
+		if ((Sql_Table_exists($imgtbl) && (!Sql_Table_Column_Exists($imgtbl, "cksum")))
 		|| (Sql_Table_exists($msgtbl) && (!Sql_Table_Column_Exists($msgtbl, "original")))){	// Have old database tables?
 			// Drop the old tables
 			Sql_Drop_Table($imgtbl);
 			Sql_Drop_Table($msgtbl);
-			
+
 			// Flag the plugin as not intialized so that the parent will create the new tables
 			$entry = md5('plugin-inlineImagePlugin-initialised');
-			$query = sprintf("delete from %s where item='%s'", $GLOBALS["tables"]["config"], $entry); 
+			$query = sprintf("delete from %s where item='%s'", $GLOBALS["tables"]["config"], $entry);
 			Sql_Query ($query);
 			// Force reloading of config arrays, so that our parent sees the plugin
 			// as not initialized.
 			unset($_SESSION['config']);
-  			unset($GLOBALS['config']); 
+  			unset($GLOBALS['config']);
   			unset($_SESSION["dbtables"]); 	// Empty the cache that still contains our table names
-		} 
-		
+		}
+
 		parent::initialise();
 
     }
-    
+
     // The value of an attribute in an inline image placeholder
     // $str is the argument searched for the attribute
     // $att is the name of the attribute whose we are seeking
@@ -170,7 +173,7 @@ class inlineImagePlugin extends phplistPlugin
 		else
 			return $match[0];
 	}
-	
+
 	// getMimeType -- Check the extension from the original name of the file
 	// then check the file contents for type, using the temp file where we have
 	// stored the data. We would not need to do this by a way of a temp file
@@ -182,7 +185,7 @@ class inlineImagePlugin extends phplistPlugin
 			return false;
 		$type = $this->image_types[$ext];
 
-		/* Even if it's the right extension, it still might not be a genuine image */				
+		/* Even if it's the right extension, it still might not be a genuine image */
 		if (class_exists('finfo')) {
 			$info = new finfo(FILEINFO_MIME);
 			$tempary = explode(';', $info->file($tempfile));
@@ -198,7 +201,7 @@ class inlineImagePlugin extends phplistPlugin
 		}
 		return $type;
 	}
-	
+
 	private function loadTemplate($tid) {
 		if ($tid) {
     		$req = Sql_Fetch_Row_Query("select template from {$GLOBALS['tables']['template']} where id = {$tid}");
@@ -207,6 +210,18 @@ class inlineImagePlugin extends phplistPlugin
     		return '';
 	}
 
+
+	private function my_file_get_contents($src) {
+		$str='';
+		if (!$str = file_get_contents($src)){
+			//if not full URL try to add web-root path to src value
+			if(substr($src, 0, 1) != "/") {
+				$src='/'.$src;
+			}
+			$src=$this->web_root.$src;
+		}
+		return file_get_contents($src);
+	}
 
 	/* allowMessageToBeQueued
 	* called to verify that the message can be added to the queue
@@ -217,14 +232,14 @@ class inlineImagePlugin extends phplistPlugin
 	* the total size of the attached images is less than the limit specified for the
 	* plugin.
 	*/
-	function allowMessageToBeQueued($messagedata = array()) 
+	function allowMessageToBeQueued($messagedata = array())
 	{
 		$msgtbl = $GLOBALS['tables']['inlineImagePlugin_msg'];
-		
+
 		$msg = $messagedata['message'];
 		$id = $messagedata['id'];
-		
-		// We could get here if a message has been queued and then suspended for 
+
+		// We could get here if a message has been queued and then suspended for
 		// re-editing. So make sure that we have not stored any data for this message
 		$query = sprintf ("select * from %s where id=%d", $msgtbl, $id);
 		$queued_already = (Sql_Num_Rows(Sql_Query($query)) > 0);
@@ -235,7 +250,7 @@ class inlineImagePlugin extends phplistPlugin
 
 		$tempfile = $this->coderoot . 'images/tempimg.tmp';
 		$limit = getConfig("ImageAttachLimit");
-		
+
 		// Merge the message and template to gather the images
 		// Make sure that we have all parts of the message that may contain images
 		if ($messagedata["template"]) {
@@ -248,23 +263,24 @@ class inlineImagePlugin extends phplistPlugin
     		$msg = str_ireplace("[FOOTER]", $messagedata["footer"],$msg);
     	else									// Phplist always adds a footer.
     		$msg .= $messagedata["footer"]; 	// We're not constructing the message, just collecting inline image files
-				
+
 		/* Collect the inline image tags. CSS class="inline" marks inline images */
 		// The regex below matches <img .... =inline> or <img...."inline"> or <img....=inline >
 		// or <img ...."inline"/> or <img ... "inline" /> or <img ... "inline another class"... />
 		// It does not match something like <img ...."inlineclass" .../>
-		// This same regex is also used in the messageQueued() method and 
+		// This same regex is also used in the messageQueued() method and
 		// in the method parseOutgoingHTMLMessage() below.
 		if (preg_match_all('#<img[^<>]+\Winline(?:\W.*(?:/)?)?>#Ui', $msg, $match)) {
 			$total = 0;
 			foreach ($match[0] as $val) {
 
 				$src = $this->getAttribute($val, "src");
-				if (!$str = file_get_contents($src))
+				if (!$str = $this->my_file_get_contents($src)){
 					return 'Cannot access image file: ' . str_replace('<', '&lt;', $val);
+				}
 				file_put_contents($tempfile, $str);	// Create a temporary file in order to check mime type
-													// We do this only because PHP may be earlier than 5.3
-				
+									// We do this only because PHP may be earlier than 5.3
+
 				/* Is it an image file? */
 				if (!$this->getMimeType(pathinfo($src, PATHINFO_EXTENSION), $tempfile))
 					return "The URL does not reference an image file in ". str_replace('<', '&lt;', $val);
@@ -277,7 +293,7 @@ class inlineImagePlugin extends phplistPlugin
 		} else // No inline image files
 			return '';
     }
-    
+
     private function getTempFilename($filename) {
     	$fparts = pathinfo($filename);
     	$tempnm = tempnam($this->coderoot . 'images/', $fparts['filename']);
@@ -285,7 +301,38 @@ class inlineImagePlugin extends phplistPlugin
 		$tempnm .= '.' . $fparts['extension'];
 		return $tempnm;
 	}
-    	
+
+	private function make_full_uri($url,$domain,$scheme = 'http://'){
+		$parse_url=parse_url($url);
+		if(substr($parse_url['path'], 0, 1) != "/") {
+			$parse_url['path']='/'.$parse_url['path'];
+		}
+		$url=parse_url($url, PHP_URL_HOST) === null ? $domain . $parse_url['path'] : $url;
+		$url=parse_url($url, PHP_URL_SCHEME) === null ? $scheme . $url : $url;
+		return $url;
+	}
+
+	private function try_replace($content,$original,$imagetag,$scheme = 'http://'){
+		$content = str_replace($original, $imagetag, $content,$found);
+		if ($found==0) {
+			if (preg_match_all('#<img[^<>]+\Winline(?:\W.*(?:/)?)?>#Ui', $original, $match)) {
+				foreach ($match[0] as $val) {
+					$src = $this->getAttribute($val, "src");
+					//try Full URI
+					$domain=getConfig("domain");
+					$uri=$this->make_full_uri($src,$domain,$scheme);
+					$tmp_orig_new=str_replace($src, $uri, $original);
+					$content = str_replace($tmp_orig_new, $imagetag, $content,$found1);
+					//try https
+					if ($found1==0){
+						$content=$this->try_replace($content,$original,$imagetag,'https://');
+					}
+				}
+			}
+		}
+		return $content;
+	}
+
 	/* messageQueued
 	* called when a message is placed in the queue
 	* @param integer id message id
@@ -294,18 +341,18 @@ class inlineImagePlugin extends phplistPlugin
 	* This is where we queue the inline images associated with the message in the plugin's
 	* own database tables.
 	*/
-            	
+
 	function messageQueued($id) {
 		$imgtbl = $GLOBALS['tables']['inlineImagePlugin_image'];
 		$msgtbl = $GLOBALS['tables']['inlineImagePlugin_msg'];
-		
+
 		$msgdata = loadMessageData($id);
 		$msg = $msgdata['message'];
-		
+
 		$query = sprintf ("select * from %s where id=%d", $msgtbl, $id);
 		$queued_already = (Sql_Num_Rows(Sql_Query($query)) > 0);	// Forwarding or requeueing
 																	// a message already sent
-		
+
 		// Merge the message and template to check the images
 		// Make sure that we have all parts of the message that may contain images
 		if ($msgdata["template"]) {
@@ -313,19 +360,19 @@ class inlineImagePlugin extends phplistPlugin
 			if (strpos($template, "[CONTENT]"))
     			$msg = str_replace("[CONTENT]",$msg, $template);
     	}
-    	
+
     	if (strpos($msg, "[FOOTER]") !== false)
     		$msg = str_ireplace("[FOOTER]", $msgdata["footer"],$msg);
 		else									// Phplist always adds a footer.
     		$msg .= $msgdata["footer"]; 	// We're not constructing the message, just collecting inline image files
-		
+
 		// Collect the inline image tags
     	preg_match_all('#<img[^<>]+\Winline(?:\W.*(?:/)?)?>#Ui', $msg, $match);
 
 		//Store everything needed for rapid processing of messages
 		foreach ($match[0] as $val) {
 			$src = $this->getAttribute($val, "src");
-    		$fcontents = file_get_contents($src);
+    		$fcontents = $this->my_file_get_contents($src);
     		$hsh = sha1($fcontents);	// Use a checksum to distinguish different files with same name
     		$filename = basename ($src);
     		$query = sprintf("select imgid, cid, local_name from %s where file_name='%s' and cksum='%s'", $imgtbl, $filename, $hsh);
@@ -339,8 +386,8 @@ class inlineImagePlugin extends phplistPlugin
     				$query = sprintf("select imgid from %s where file_name='%s' and cksum='%s'", $imgtbl, $filename, $hsh);
     				$row = Sql_Fetch_Row_Query($query);
     				$imgid = $row[0];
-    			}		
-    		} else if (!file_exists($row[2])) { // We've had the image before, but it has 
+    			}
+    		} else if (!file_exists($row[2])) { // We've had the image before, but it has
     											// been stored in the plugin only as a temporary file
     											// which may have been deleted
     			$localfile = $this->getTempFilename($filename);
@@ -351,7 +398,7 @@ class inlineImagePlugin extends phplistPlugin
     			$imgid = $row[0];
     			$cid = $row[1];
     		}
-    		
+
     		if (!$queued_already) {	// Associate the image with the message
     			$srcstr = $this->getAttribute($val, "src", 0);
     			$imgtag = str_replace($srcstr, 'src="cid:' . $cid . '"', $val);
@@ -359,13 +406,13 @@ class inlineImagePlugin extends phplistPlugin
 				Sql_Query($query);
 				// We may have multiple copies of an image in a message. We do store those multiple
 				// copies here. That's ok because PHPmailer does not seem to want to attach more than
-				// one copy of the image to the message, even if the plugin attempts to do such 
-				// an attachment more that once. 
+				// one copy of the image to the message, even if the plugin attempts to do such
+				// an attachment more that once.
 			}
     	}
-	
-  	} 
-  	
+
+  	}
+
   	/* messageReQueued
   	* called when a message is placed back in the queue
 	* @param integer id message id
@@ -375,17 +422,17 @@ class inlineImagePlugin extends phplistPlugin
   	function messageReQueued($id) {
   		$this->messageQueued($id);	// We don't care whether the message has been sent before
   	}
-  	
+
   	// Set $this->curid and cache the data necessary to formulate the outgoing message
   	// with inline image attachments
   	function loadImageCache($messagedata = array()) {
   		$this->curid = $messagedata['id'];
-  		$this->cache[$this->curid] = array(); 	// Make sure that the cache defined 
+  		$this->cache[$this->curid] = array(); 	// Make sure that the cache defined
   												// even if no images
-  		
+
   		$msgtbl = $GLOBALS['tables']['inlineImagePlugin_msg'];
   		$imgtbl = $GLOBALS['tables']['inlineImagePlugin_image'];
-  		
+
 		$query = sprintf('select original, imagetag, cid, type, file_name, local_name from %s natural join %s where id = %d', $msgtbl, $imgtbl, $this->curid);
   		$result = Sql_Query($query);
   		$i = 0;
@@ -394,9 +441,9 @@ class inlineImagePlugin extends phplistPlugin
   			$this->cache[$this->curid][$i]['contents'] = file_get_contents($row['local_name']);
   			$i++;
   		}
-  	} 
-  	
-  	
+  	}
+
+
   	/*
 	* campaignStarted
 	* called when sending of a campaign starts
@@ -408,9 +455,9 @@ class inlineImagePlugin extends phplistPlugin
 
   	function campaignStarted($messagedata = array()) {
   		$this->loadImageCache($messagedata);
-  	} 
-  	
-	/* 
+  	}
+
+	/*
    	* parseOutgoingTextMessage
    	* @param integer messageid: ID of the message
    	* @param string  content: entire text content of a message going out
@@ -424,7 +471,7 @@ class inlineImagePlugin extends phplistPlugin
     	return $content;
   	}
 
-  	/* 
+  	/*
    	* parseOutgoingHTMLMessage
    	* @param integer messageid: ID of the message
   	* @param string  content: entire text content of a message going out
@@ -447,15 +494,17 @@ class inlineImagePlugin extends phplistPlugin
   				$this->loadImageCache($msgdata);
   			}
   		}
-  		
-  		// Replace all the image tags for inline images with tags pointing to the attached files	
+
+  		// Replace all the image tags for inline images with tags pointing to the attached files
   		$n = count($this->cache[$messageid]);
     	for ($i = 0; $i < $n; $i++) { // And replace them
-  				$content = str_replace($this->cache[$messageid][$i]['original'], $this->cache[$messageid][$i]['imagetag'], $content);
+  				//$content = str_replace($this->cache[$messageid][$i]['original'], $this->cache[$messageid][$i]['imagetag'], $content);
+					//try to replace, while phplist makes the url to an uri
+					$content=$this->try_replace($content,$this->cache[$messageid][$i]['original'],$this->cache[$messageid][$i]['imagetag']);
   		}
     	return $content;
     }
-  	
+
   	 /* processQueueStart
    	* called at the beginning of processQueue, after the process was locked
    	* @param none
@@ -465,20 +514,20 @@ class inlineImagePlugin extends phplistPlugin
   		$this->processing_queue = true;
   		$this->forwarding_message = false;
   	}
-  	
+
   	/* messageQueueFinished
    	* called when a sending of the queue has finished
    	* @return null
    	*/
   	function messageQueueFinished() {
   		$this->processing_queue = false;
-  		
-  		// Clean out the temporary files in the image directory, if the size has 
+
+  		// Clean out the temporary files in the image directory, if the size has
   		// gotten to be too large.
   		$imgdir = $this->coderoot . "images/";
   		$files = scandir($imgdir);
   		$sum = 0;
-  		foreach ($files as $afile) 
+  		foreach ($files as $afile)
   			if (!is_dir($afile))
   				$sum += filesize($afile);
   		if ($sum >= 1000 * $this->imgdirlimit)
@@ -492,19 +541,19 @@ class inlineImagePlugin extends phplistPlugin
    	*
 	* @param object $mail
    	* @return array (headeritem => headervalue)
-   	* 
+   	*
    	* We do not use this method primarily for creating headers. Instead we use it
    	* for adding image attachments to the $mail object.
    	*
    	*/
   	function messageHeaders($mail) {
-  	
+
   		if ((!$this->processing_queue) && (!$this->forwarding_message)) 	// Administrative message?
   			return;
-  		
+
   		$imgs = $this->cache[$this->curid];
   		for($i=0; $i< sizeof($imgs); $i++) {
-  		
+
   			// Borrowed from the add_html_image() method of the PHPlistMailer class
   			if (method_exists($mail,'AddEmbeddedImageString')) {
         		$mail->AddEmbeddedImageString($imgs[$i]['contents'], $imgs[$i]['cid'], $imgs[$i]['file_name'], $mail->encoding, $imgs[$i]['type']);
@@ -527,7 +576,7 @@ class inlineImagePlugin extends phplistPlugin
         		logEvent("phpMailer needs patching to be able to use inline images");
        			print Error("phpMailer needs patching to be able to use inline images");
         	}
-        } 
+        }
         return '';
   	}
 }
